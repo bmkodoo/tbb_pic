ffa2466c3db5ee48db6d3428a027639ee4b23810
kodoo <bm.kodoo@gmail.com>
2016-12-11 15:21:24 +0300

    tbb hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "tbb/task_group.h"
+
+using namespace tbb;
+
+int Fib(int n) {
+    if (n < 2) {
+        return n;
+    } else {
+        int x, y;
+        task_group g;
+        g.run([&] { x = Fib(n - 1); }); // spawn a task
+        g.run([&] { y = Fib(n - 2); }); // spawn another task
+        g.wait();                // wait for both tasks to complete
+        return x + y;
+    }
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << Fib(10) << std::endl;
     return 0;
 }
