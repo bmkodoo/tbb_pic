9fea9df194933219585448255283368aeaa2c44f
kodoo <bm.kodoo@gmail.com>
2016-12-27 22:38:30 +0300

    graph + inverse node + utils

--- main.cpp
+++ main.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
 #include "tbb/task_group.h"
 #include "tbb/tbb.h"
+#include "utils.h"
+#include "flow.h"
 #include <Magick++.h>
 
 using namespace tbb;
-
-int Fib(int n) {
-    if (n < 2) {
-        return n;
-    } else {
-        int x, y;
-        task_group g;
-        g.run([&] { x = Fib(n - 1); }); // spawn a task
-        g.run([&] { y = Fib(n - 2); }); // spawn another task
-        g.wait();                // wait for both tasks to complete
-        return x + y;
-    }
-}
-
-
 using namespace std;
 using namespace tbb::flow;
 
-//graph g;
-//continue_node<continue_msg> hello(
-//        g,
-//        [](const continue_msg &) {
-//            cout << "Hello";
-//        }
-//);
-//continue_node<continue_msg> world(
-//        g,
-//        [](const continue_msg &) {
-//            cout << " World\n";
-//        }
-//);
-//make_edge(hello, world);
-//hello.try_put(continue_msg());
-//g.wait_for_all();
-//return 0;
 
-int main() {
-    auto image = new Magick::Image();
-    image->read("/home/kodoo/Pictures/image002.jpg");
-    image->write("/home/kodoo/Pictures/image002_2.jpg");
+int main(int argc, const char * argv[]) {
+    try {
+        CommandLineInput args;
+        vector<string> images;
+        images.push_back("/home/kodoo/git/tbb_lab/in/YurijGagarin.jpg");
+        args.images = images;
+        args.output = "/home/kodoo/git/tbb_lab/out/";
+        args.inputBrightness = 100;
+        startFlow(args);
+    } catch (string ex) {
+        cerr<<ex<<endl;
+    }
+    return 0;
 }
